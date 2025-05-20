f4d3b95a36e32443a43144966fc25514525c6ba9
PreethiND <preethi.nd@walmart.com>
2025-05-20 17:22:22 +0530

    Swift constants and variables

---
+++ ConstantsAndVariables/Constants.swift
@@ -0,0 +1,14 @@
+// Swift Constants
+// Constants in Swift are declared using the `let` keyword. Once assigned, their values cannot be changed.
+// They ensure immutability, improve code safety, and make the code more readable and maintainable.
+
+// Example:
+let day = "Monday"
+let dailyTemperature = 75
+print("Today is \(day). Raise and Shine!")
+print("The temperature on \(day) is \(dailyTemperature)°F")
+
+// Key Points:
+// - Constants must be initialized at the time of declaration.
+// - They are ideal for values that should remain unchanged, such as configuration settings or fixed data.
+
